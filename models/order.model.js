674ed3f8146640289eea77d992512700e72dc08d
qrapidnow <qrapidnow@gmail.com>
2024-07-14 17:32:33 +0530

    for quantity

--- models/order.model.js
+++ models/order.model.js
@@ -9,7 +9,7 @@ const OrderSchema = new Schema({
     {
       name: { type: String, required: true },
       price: { type: Number, required: true },
-      quantity: { type: Number, required: true }, // Add quantity field here
+      quantity: { type: Number, required: true } // Quantity field for each item
     }
   ],
   createdAt: { type: Date, default: Date.now }
--- routes/order.router.js
+++ routes/order.router.js
@@ -2,7 +2,6 @@ const express = require('express');
 const { saveOrder } = require('../controllers/order.controller');
 const router = express.Router();
 
-// Route to save order
 router.post('/orders', saveOrder);
 
 module.exports = router;
